aba2ceb1177b0cbcdc49ea0c1cefa09f8b75bc2e
c0nfirm <julian_rathenow91@hotmail.de>
2024-11-24 18:10:04 +0100

    grundgerüst
    Prototyp Methoden für ein einheitliches Grund Gerüst (alles gerne ändern wenn ich Vorschläge habt)

--- Network Shell/Work/cli/client.c
+++ Network Shell/Work/cli/client.c
@@ -4,6 +4,28 @@
 #define PORT 9000
 #define HOST "127.0.0.1"
 
+int shell_exit();
+
+/*Client Put*/
+int c_put(char **args);
+/*Client Get*/
+int c_get(char **args);
+/*start server connection*/
+int launch(char **args, int argcount);
+/*reads input line from stdin and returns it*/
+char *read_line(void);
+/*  Splits the input line into tokens
+    returns a null terminated array of tokens
+*/
+char **split_line(char *line, int *argcount);
+/*shell commands*/
+int run(char **args, int argccount);
+
+static inline void die(const char *msg){
+	perror(msg);
+	exit(-1);
+}
+
 int main() {
 	getc(stdin);
 	printf("Client Exit\n");
--- Network Shell/Work/srv/server.c
+++ Network Shell/Work/srv/server.c
@@ -4,7 +4,64 @@
 
 #define PORT 9000
 
-int main() {
+/*Server data structure: keeps track of how many workers are
+	active, and whether a shutdown has been requested*/
+typedef struct{
+	int num_workers;
+	int shutdown_requested;
+
+	clock_t last_message_timestamp;
+	char last_message[1024];
+
+	pthread_mutex_t lock;
+	pthread_cond_t cond;
+} Server;
+
+/*Data used by a worker that chats with a specific client*/
+typedef struct{
+	Server *s;
+	int client_fd;
+} Worker;
+
+/*Server task - waits for incoming connections, and spawns workers*/
+void *server(void *arg);
+/*Worker task - chats with clients*/
+void *worker(void *arg);
+
+static inline void die(const char *msg){	
+	perror(msg);
+	exit(-1);
+}
+
+
+/*reads input line from stdin and returns it*/
+char *read_line(void);
+/*  Splits the input line into tokens
+    returns a null terminated array of tokens
+*/
+char **split_line(char *line);
+
+/*         Change Directory Command
+    args[0] =  "cd", args[1] = directory
+    returns 1 to continue execution
+*/
+int shell_cd(char **args);
+/*shell launch*/
+int launch(char **args);
+
+/*Server - Get*/	
+int s_get(char **args, FILE *fh);
+/*Server - Put*/
+int s_put(char **args, FILE *fh);
+int s_wall(char **args, Server *s);
+
+/Server - Commands/
+int execute(char **args, FILE *write_fh, FILE *read_fh, Server *s);
+
+void *server(void *arg);
+void *server(void *arg);
+
+int main(int argc, char **argv) {
 	while (1) {
 		printf("[srv]: idle\n");
 		sleep(2);
