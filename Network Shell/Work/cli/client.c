72b3e2c22cf0da587baaec5d461da04db05262ce
c0nfirm <julian_rathenow91@hotmail.de>
2024-11-25 11:30:20 +0100

    simple c > s setup
    added simple Client > Server Connection (echo Server)  + Comments

--- Network Shell/Work/cli/client.c
+++ Network Shell/Work/cli/client.c
@@ -1,11 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #define PORT 9000
 #define HOST "127.0.0.1"
 
-int shell_exit();
+/*Error Catch*/
+static inline void die(const char *msg){
+	perror(msg);
+	exit(-1);
+}
 
+int main(){
+	/*connection to server*/
+	struct sockaddr_in addr = {
+		.sin_family = AF_INET,
+		.sin_port = htons(PORT),
+		.sin_addr.s_addr = inet_addr(HOST)
+	};
+
+	char buf[256];
+	int cfd;
+
+	if((cfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		die("Couldn't open the socket!");
+	
+	if(connect(cfd, (struct sockaddr*) &addr, sizeof(addr)) < 0)
+		die("Couldn't connect to socket!");
+
+	/*commands and den server*/
+	for(int i = 0; i < 5; i++){
+		if(write(cfd, "Ping", 4) < 0)
+			die("Couldn't send message!");
+		printf("[send] Ping\n");
+
+		if(read(cfd, buf, sizeof(buf)) < 0)
+			die("Couldn't recieve message!");
+		printf("[recv] %s\n", buf);		
+	}	
+	
+	close(cfd);
+	return 0;
+}
+
+int shell_exit();
 /*Client Put*/
 int c_put(char **args);
 /*Client Get*/
@@ -20,15 +61,3 @@ char *read_line(void);
 char **split_line(char *line, int *argcount);
 /*shell commands*/
 int run(char **args, int argccount);
-
-static inline void die(const char *msg){
-	perror(msg);
-	exit(-1);
-}
-
-int main() {
-	getc(stdin);
-	printf("Client Exit\n");
-	
-	return 0;
-}
--- Network Shell/Work/srv/server.c
+++ Network Shell/Work/srv/server.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <unistd.h>
+#include <netinet/in.h>
 
 #define PORT 9000
+
+/*Error Catch*/
+static inline void die(const char *msg){	
+	perror(msg);
+	exit(-1);
+}
+
+int main(){
+	struct sockaddr_in s_addr, c_addr;
+	int sockopt = 1;
+	socklen_t sad_sz = sizeof(struct sockaddr_in);
+	int sfd, cfd;
+	ssize_t bytes;
+	char buf[256];
+
+	/*Connection handling between s and c*/
+	s_addr.sin_family =AF_INET;
+	s_addr.sin_port = htons(PORT);
+	s_addr.sin_addr.s_addr = INADDR_ANY;
+
+	if((sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		die("Couldn't open the socket!");
+	setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, (char*) &sockopt, sizeof(sockopt));
+
+	if(bind(sfd, (struct sockaddr*) &s_addr, sad_sz) < 0)
+		die("Couldn't bind socket!");
+
+	if(listen(sfd, 1) < 0)
+		die("Couldn't listen to the socket!");
+
+	cfd = accept(sfd, (struct sockaddr*) &c_addr, &sad_sz);
+	if(cfd < 0)
+		die("Couldn't accept incoming connection!");
+
+	/*"Execute" client queries*/
+	while((bytes = read(cfd, buf, sizeof(buf))) != 0){
+		if(bytes < 0)
+			die("Couldn't recieve message!");
+
+		if(write(cfd, buf, bytes) < 0)
+			die("Couldn't send message!");	
+	}
+
+	close(cfd);
+	close(sfd);				
+	return 0;
+}
 
 /*Server data structure: keeps track of how many workers are
 	active, and whether a shutdown has been requested*/
@@ -28,12 +77,6 @@ void *server(void *arg);
 /*Worker task - chats with clients*/
 void *worker(void *arg);
 
-static inline void die(const char *msg){	
-	perror(msg);
-	exit(-1);
-}
-
-
 /*reads input line from stdin and returns it*/
 char *read_line(void);
 /*  Splits the input line into tokens
@@ -55,17 +98,8 @@ int s_get(char **args, FILE *fh);
 int s_put(char **args, FILE *fh);
 int s_wall(char **args, Server *s);
 
-/Server - Commands/
+/*Server - Commands*/
 int execute(char **args, FILE *write_fh, FILE *read_fh, Server *s);
 
 void *server(void *arg);
 void *server(void *arg);
-
-int main(int argc, char **argv) {
-	while (1) {
-		printf("[srv]: idle\n");
-		sleep(2);
-	}
-	
-	return 0;
-}
