dd49f0845620ce82e35b064b4f63be9f8462d1c1
c0nfirm <julian_rathenow91@hotmail.de>
2024-12-02 21:39:52 +0100

    new try put
    swfdc

--- Network Shell/Work/cli/client.c
+++ Network Shell/Work/cli/client.c
@@ -13,39 +13,46 @@
 #define HOST "127.0.0.1"
 #define MAX 128
 /*file size placeholder! Maybe get filesize and allocate it on the fly*/
-#define SIZE 1024
+#define FMAX 1024
 
-void c_sendF(int sockD){
-	char data[SIZE] = {0}, buf[MAX];
-	FILE *fp;
-
-	printf("[-] buf: %s\n", buf);
-	recv(sockD, buf, sizeof(buf), 0);
-	printf("[-] buf: %s\n", buf);
-	strtok(buf, "\n");
-	printf("Data: %s\n", buf);
-	fp = fopen(buf, "r");
-	if(fp == NULL){
-		perror("[-] Error reading file");
-		exit(1);
+void read_f(char *f_name, int c_cocked){
+	FILE *fd;
+	fd = fopen(f_name, "r"); /*read*/
+	char f_buf[FMAX];
+	if(fd == NULL){
+		printf("[-]Error: Could not open File!\n");
+		exit(-1);
 	}
 
-	printf("&>Uploading file to server... \n");
-	printf("Data: %s\n", data);
-	while(fgets(data, SIZE, fp) != NULL){
-		printf("[-] data: %s\n", data);
-		if(send(sockD, data, sizeof(data), 0) == -1){
-			printf("[-] error\n");
-			perror("[-] Error in sending file.");
-			exit(1);
+	bzero(f_buf, FMAX);
+	/*reads file line and send's it to the client*/
+	while(fgets(f_buf, FMAX, fd) != NULL){
+		printf("[+] bf_buf:  %s\n", f_buf);
+		send(c_cocked, f_buf, sizeof(FMAX), 0);
+		printf("[+] af_buf:  %s\n", f_buf);
+	}
+	fclose(fd);
+}
+
+void write_f(char *f_name, FILE *fd, int c_socked){
+	fd = fopen(f_name, "w"); /*write*/
+	char f_buf[FMAX];
+	if(fd == NULL){
+		printf("[-]Error: Could not open File!\n");
+		exit(-1);
+	}
+
+	for(;;){
+		recv(c_socked, f_buf, sizeof(FMAX), 0);
+		if(fprintf(fd,"%s", f_buf) < 0){
+			break;
 		}
-		bzero(data, SIZE);
 	}
-	fclose(fp);
+	fclose(fd);
 }
 
 void func (int sockD){
-	char buf[MAX], tmp[MAX];
+	char buf[MAX];
 	int n;
 
 	/*infinite loop*/
@@ -53,19 +60,22 @@ void func (int sockD){
 		printf("$> ");
 		/*clears any data in buf by overwriting it with zero "\0" */
 		bzero(buf, sizeof(buf));
-		bzero(tmp, sizeof(tmp));
 		
 		n = 0;
 		while((buf[n++] = getchar()) != '\n');
 
 		/*catch put*/
 		if(memcmp("put ", buf,  4) == 0){
+			/*send imput to server*/
 			send(sockD, buf, sizeof(buf), 0);
 			printf("Msg to sev: %s", buf);
 			bzero(buf, MAX);
-			c_sendF(sockD);
+			
+			/*gets's filename to upload to server*/
+			recv(sockD, buf, sizeof(buf), 0);
+			read_f(buf, sockD);
+			bzero(buf, MAX);
 
-			bzero(buf, sizeof(buf));
 			continue;
 		}
 
--- Network Shell/Work/srv/server.c
+++ Network Shell/Work/srv/server.c
@@ -4,50 +4,64 @@
 #include <netinet/in.h> /*struct storing addr info*/
 #include <sys/socket.h> /*socket api*/
 #include <sys/types.h>
-#include <arpa/inet.h>	/*inet_addr()*/
-#include <unistd.h>		/*read(), write(), close()*/
+#include <arpa/inet.h> /*inet_addr()*/
+#include <unistd.h>	   /*read(), write(), close()*/
 #include <string.h>
 
 #define PORT 9000
 #define HOST "127.0.0.1"
 #define MAX 128
-#define SIZE 1024
+#define FMAX 1024
 
-/*void s_writeF(int sockFD){
-	int n;
-	FILE *fp;
-	char name[MAX], path[MAX], cuurr_dir[MAX], *tmp = "test.txt";
-	char data[SIZE];
-	printf("[-] write func\n");
-	fp = fopen(tmp, "wb");
-	printf("[-] fopen\n");
-	printf("[-] TEST_1\n");
-	while(1){	
-		printf("[-] TEST_3\n");
-		recv(sockFD, data, sizeof(data), 0);
-		printf("[-]Data : %s\n", data);
-		if(fputs(data, fp)== EOF){break;}
-		printf("[-]recv = %i\n", n);
-		if(n <= 0){
-			printf("[-] FINISH\n");
+void read_f(char *f_name,FILE *fd, int c_cocked){
+	fd = fopen(f_name, "r"); /*read*/
+	char f_buf[FMAX];
+	if(fd == NULL){
+		printf("[-]Error: Could not open File!\n");
+		exit(-1);
+	}
+
+	/*reads file line and send's it to the client*/
+	while(fgets(f_buf, FMAX, fd) != 0){
+		send(c_cocked, f_buf, sizeof(FMAX), 0);
+		/*Do something with files*/
+	}
+	fclose(fd);
+}
+void write_f(char *f_name, int c_socked){
+	FILE *fd;
+	char f_buf[FMAX];
+	fd = fopen(f_name, "w"); /*write*/
+	bzero(f_buf, FMAX);
+
+	if(fd == NULL){
+		printf("[-]Error: Could not open File!\n");
+		exit(-1);
+	}
+
+	for(;;){
+		printf("[+] bf_buf:  %s\n", f_buf);
+		
+		recv(c_socked, f_buf, sizeof(f_buf), MSG_FIN);
+		printf("[+] mf_buf:  %s\n", f_buf);
+		if(fprintf(fd,"%s", f_buf) < 0){
 			break;
 		}
-		printf("[-] TEST_4\n");
-		fprintf(fp, "%s", data);
-		bzero(data, SIZE);
+		printf("[+] af_buf:  %s\n", f_buf);
+			
+		bzero(f_buf, FMAX);
 	}
-	fclose(fp);
-	printf("File recieved\n");
-}*/
+	fclose(fd);
+}
 
 void func(int c_socked){
 	printf("test_func_start\n");
 	char buf[MAX], tmp[MAX], home_dir[MAX];
 	FILE *out;
-	if(getcwd(home_dir, MAX) == 0){}
-	
+	if (getcwd(home_dir, MAX) == 0){}
+
 	/*infinite loop*/
-	for(;;){
+	for (;;){
 		/*clears any data in buf by overwriting it with zero "\0" */
 		bzero(buf, MAX);
 		/*read msg from client*/
@@ -55,15 +69,17 @@ void func(int c_socked){
 		printf("FIRST RECV: %s\n", buf);
 
 		/* ls - command from client*/
-		if(strncmp("ls\n",buf, 3) == 0 || memcmp("ls ", buf, 3) == 0){
+		if (strncmp("ls\n", buf, 3) == 0 || memcmp("ls ", buf, 3) == 0){
 			printf("IN LS\n");
 			out = popen("ls -a", "r");
 
 			bzero(buf, MAX);
-			if(out == NULL){fputs("POPEN: Failed to execute command ls.\n", stderr);}
+			if (out == NULL){
+				fputs("POPEN: Failed to execute command ls.\n", stderr);
+			}
 			else{
 				printf("ELSE LS\n");
-				while(fgets(tmp, MAX-1, out) != NULL){
+				while (fgets(tmp, MAX - 1, out) != NULL){
 					strcat(buf, tmp);
 				}
 			}
@@ -72,26 +88,27 @@ void func(int c_socked){
 			continue;
 		}
 		/* cd <path> - command from client*/
-		if(memcmp("cd ",buf, 3) == 0 || memcmp("cd\n", buf, 3) == 0){
+		if (memcmp("cd ", buf, 3) == 0 || memcmp("cd\n", buf, 3) == 0){
 			printf("NORMAL CD\n");
 			char path[MAX];
 			strcpy(path, buf);
 			size_t len = strlen(path);
-			memmove(path, path+3, len - 3 + 1);
+			memmove(path, path + 3, len - 3 + 1);
 			printf("abgeschnitten: %s\n", path);
 			strtok(path, "\n");
 
 			bzero(buf, MAX);
 
-			if(chdir(path) == 0){
-				if(getcwd(buf, MAX) != NULL){
+			if (chdir(path) == 0){
+				if (getcwd(buf, MAX) != NULL){
 					printf("cwd: %s\n", buf);
 					strcat(buf, "\n");
 					send(c_socked, buf, sizeof(buf), 0);
 				}
-			}else{
-				if(chdir(home_dir) == 0){
-					if(getcwd(buf, MAX) != NULL){
+			}
+			else{
+				if (chdir(home_dir) == 0){
+					if (getcwd(buf, MAX) != NULL){
 						printf("cwd: %s\n", buf);
 						strcat(buf, "\n");
 						send(c_socked, buf, sizeof(buf), 0);
@@ -101,50 +118,51 @@ void func(int c_socked){
 			continue;
 		}
 		/* get <file> - command from client*/
-		if(strncmp("get ",buf, 4) == 0){
-  			bzero(buf, MAX);
+		if (memcmp("get ", buf, 4) == 0){
+			bzero(buf, MAX);
 			strcpy(buf, "get");
 			send(c_socked, buf, sizeof(buf), 0);
 			continue;
 		}
 		/* put <file> - command from client*/
-		if(strncmp("put ",buf, 4) == 0){
-			printf("[-] buf: %s\n", buf);
-			FILE *fd;
-			char path[MAX], *tmp = "text.txt";
-			fd = fopen(tmp, "w");
-			strcpy(path, buf);
-			printf("[-] path: %s\n", path);
-			size_t len = strlen(path);
-			memmove(path, path+4, len - 4 + 1);
-			printf("abgeschnitten: %s\n", path);
-			strtok(path, "\n");
-			printf("[-] buf: %s\n", path);
-			send(c_socked, path, sizeof(path), 0);
+		if (memcmp("put ", buf, 4) == 0){
+			char f_path[MAX];
+
+			/*cut prefix 'put ' from string*/
+			strcpy(f_path, buf);
+			size_t len = strlen(f_path);
+			memmove(f_path, f_path + 4, len - 4 + 1);
+			printf("abgeschnitten: %s\n", f_path);
+			strtok(f_path, "\n");
+			printf("[+] f_path:  %s\n", f_path);
 			bzero(buf, MAX);
 
-			while (1){
-				recv(c_socked, buf, sizeof(buf), 0);
-				printf("[-]Buf : %s\n", buf);
-				fprintf(fd, "%s", buf);
-			}
-			printf("File recieved\n");
+			/*send only filename/path back to client*/
+			printf("[+] f_path:  %s\n", f_path);
+			send(c_socked, f_path, sizeof(f_path), 0);
+			printf("[+] f_path:  %s\n", f_path);
+			/*writes from client file to server file*/
+			write_f(f_path, c_socked);
+
+			bzero(buf, MAX);
+			printf("File received... \n");
 			continue;
 		}
 		/* ./<prog> - command from client*/
-		if(strncmp("./",buf, 2) == 0){
+		if (strncmp("./", buf, 2) == 0){
 			bzero(buf, MAX);
 			strcpy(buf, "./<prog>");
 			send(c_socked, buf, sizeof(buf), 0);
 			continue;
 		}
 		/* exit - command from client*/
-		if(strncmp("exit\n",buf, 5) == 0){
+		if (strncmp("exit\n", buf, 5) == 0){
 			bzero(buf, MAX);
 			strcpy(buf, "exit");
 			send(c_socked, buf, sizeof(buf), 0);
 			break;
-		}else{
+		}
+		else{
 			bzero(buf, MAX);
 			strcpy(buf, "Falsche oder Leere eingabe!\n");
 			send(c_socked, buf, sizeof(buf), 0);
@@ -152,17 +170,16 @@ void func(int c_socked){
 	}
 }
 
-int main(int argc, char const* argv[]){
+int main(int argc, char const *argv[]){
 	/*create server socket similar to client socket*/
 	int servSockD = socket(AF_INET, SOCK_STREAM, 0);
-	
+
 	/*server console input*/
 	char m_buf[MAX];
 	int n;
 
 	/*data sent to client*/
 	char serMsg[255] = " \'Hello Client\' ", strData[255];
-	
 
 	/*define server addr*/
 	struct sockaddr_in serv_Addr;
@@ -171,14 +188,13 @@ int main(int argc, char const* argv[]){
 	serv_Addr.sin_addr.s_addr = inet_addr(HOST);
 
 	/*bind socket to specified IP and Port*/
-	bind(servSockD, (struct sockaddr*) &serv_Addr, sizeof(serv_Addr));
+	bind(servSockD, (struct sockaddr *)&serv_Addr, sizeof(serv_Addr));
 
-	for(;;){
+	for (;;){
 		/*listen for connections*/
 		printf("Waiting for connection...\n");
 		listen(servSockD, 1);
-		
-	
+
 		/*INT to hold client socket*/
 		int c_socket = accept(servSockD, NULL, NULL);
 
@@ -196,8 +212,9 @@ int main(int argc, char const* argv[]){
 		printf("If you want to close the server type 'exit'!\n");
 		printf("Else enter any Input\n");
 		n = 0;
-		while((m_buf[n++] = getchar()) != '\n');
-		if(strncmp("exit\n",m_buf, 5) == 0){
+		while ((m_buf[n++] = getchar()) != '\n')
+			;
+		if (strncmp("exit\n", m_buf, 5) == 0){
 			printf("Server Closed!\n");
 			bzero(m_buf, MAX);
 			break;
